684b2f9635c1ac8cf2711c5ed9ea3db2ea1d9213
Filipe de Medeiros Santos <filipemedeiros2013@gmail.com>
2023-06-24 14:05:08 -0300

    Updating game constants

--- tapatan/constants.py
+++ tapatan/constants.py
@@ -5,6 +5,8 @@ N = 3
 EMPTY = 0
 BLACK = 1
 WHITE = 2
+START = 3
+END   = 4
 
 MOVES =  [[[(0, 1), (1, 0), (1, 1)],
            [(0, 0), (0, 2), (1, 1)],
@@ -31,6 +33,7 @@ WINNING_POSITIONS = [[(0, 0), (0, 1), (0, 2)],
 
 COLOR_FONT = 0, 0, 0
 COLOR_GRID = 184, 134, 11
+COLOR_EMPH = 135, 206, 235
 
 # Dimensions
 
@@ -39,16 +42,22 @@ font_size = 20
 
 spacing = 10
 spacing_lateral = 15
-spacing_grid_lateral = 25
-spacing_grid_top = 60
+
+spacing_grid_top   = 60
+spacing_grid_left  = 25
+spacing_grid_right = width - spacing_grid_left
 spacing_button_bottom = height * 3 / 4
 
-grid = width - 2 * spacing_grid_lateral
-
-size_piece = grid / (2 * N)
-spacing_piece_center = size_piece / 2
-radius_piece_empty   = size_piece / 4
-
-w_button = h_button = 35
-w_button_play, h_button_play = width - 2 * spacing_lateral, h_button
+grid = width - 2 * spacing_grid_left
 w_grid_line = 5
+
+side_piece = grid / (2 * N)
+size_piece = side_piece, side_piece
+spacing_piece_center = side_piece / 2
+radius_piece_empty   = side_piece / 4
+radius_emphasis      = spacing_piece_center
+
+size_button       = w_button, h_button = 35, 35
+size_button_play  = w_button_play, h_button_play = width - 2 * spacing_lateral, h_button
+size_button_empty = font_size * 4, font_size * 3 / 2
+size_message_win  = grid, grid / 3
