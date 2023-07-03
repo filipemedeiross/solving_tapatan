e43d9ee962f819e31fd0bf7e3f066b51830d50f0
Filipe de Medeiros Santos <filipemedeiros2013@gmail.com>
2023-07-03 20:27:56 -0300

    New version of the game logic

--- tapatan/logic_game.py
+++ tapatan/logic_game.py
@@ -1,13 +1,11 @@
-import numpy as np
-from .constants import N, EMPTY, BLACK, WHITE, MOVES, WINNING_POSITIONS
+from .constants import BLACK, WHITE
+from .formulations import new_grid, update_grid, move, win
 
 
 # Class that implements the tapatan logic
 class TapatanGrid:
     def __init__(self):
-        self.__grid   = self.new_grid()
-        self.__moves  = MOVES
-        self.__winpos = WINNING_POSITIONS
+        self.__grid  = self.new_grid()
 
     def __str__(self):
         return '  -  '.join(map(str, self.grid[0])) + '\n' + \
@@ -19,43 +17,18 @@ class TapatanGrid:
     def __getitem__(self, args):
         return self.grid[args]
     
-    def new_grid(self):
-        grid = np.zeros((N, N), dtype='int8')
-
-        grid[0, 0] = grid[1, 2] = grid[2, 0] = BLACK
-        grid[0, 2] = grid[1, 0] = grid[2, 2] = WHITE
-
-        return grid
+    @staticmethod
+    def new_grid():
+        return new_grid()
     
     def update(self):
-        self.grid = self.new_grid()
+        update_grid(self.grid)
     
     def move(self, user, start, final):
-        x0, y0 = start
-        x1, y1 = final
-
-        assert 0 <= x0 <= 2, 'Invalid value for x in start'
-        assert 0 <= y0 <= 2, 'Invalid value for y in start'
-
-        assert 0 <= x1 <= 2, 'Invalid value for x in final'
-        assert 0 <= y1 <= 2, 'Invalid value for y in final'
-
-        assert user in [BLACK, WHITE]     , 'Invalid value for user'
-        assert self.grid[start] == user   , 'User is not in start position'
-        assert self.grid[final] == EMPTY  , 'The final position is occupied'
-        assert final in self.moves[x0][y0], 'Invalid movement'
-
-        self.grid[start] = 0
-        self.grid[final] = user
-
-        return start, final
+        return move(self.grid, user, start, final)
     
     def win(self, user):
-        for positions in self.winning_positions:
-            if all(self.grid[pos] == user for pos in positions):
-                return True
-
-        return False
+        return win(self.grid, user)
     
     @property
     def grid(self):
@@ -64,17 +37,6 @@ class TapatanGrid:
     @grid.setter
     def grid(self, grid):
         self.__grid = grid
-    
-    @property
-    def moves(self):
-        return self.__moves
-    
-    @property
-    def winning_positions(self):
-        return self.__winpos
-    
-    def user_pos(self, user):
-        return list(zip(*np.where(self.grid == user)))
 
 
 # Game prototype
